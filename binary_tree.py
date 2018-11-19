c4c39f3ac370333cdeea5dcd9d8d7886b60c1a4c
Jerry Guo <jerrguo@gmail.com>
2018-11-19 18:01:17 -0500

    Remove function for BST

--- binary_tree.py
+++ binary_tree.py
@@ -9,8 +9,63 @@ class Node():
         self.left = None
         self.right = None
 
+
     def __str__(self):
-        return f'<Node data:{{self.data}} left:{{self.left.data}} right:{{self.right.data}}>'
+        if self.left == None:
+            left_data = None
+        else:
+            left_data = self.left.data
+
+        if self.right == None:
+            right_data = None
+        else:
+            right_data = self.right.data
+
+        return f'<Node data:{self.data} left:{left_data} right:{right_data}>'
+
+
+    def __repr__(self):
+        if self.left == None:
+            left_data = None
+        else:
+            left_data = self.left.data
+
+        if self.right == None:
+            right_data = None
+        else:
+            right_data = self.right.data
+
+        return f'<Node data:{self.data} left:{left_data} right:{right_data}>'
+
+
+    def left_successor(self):
+        if self.left == None:
+            return -1
+
+        node = self.left
+        while node.right != None:
+            node = node.right
+        return node
+
+
+    def right_successor(self):
+        if self.right == None:
+            return -1
+
+        node = self.right
+        while node.left != None:
+            node = node.left
+        return node
+
+
+    def successor(self):
+        if self.left_successor():
+            return self.left_successor()
+        if self.right_successor():
+            return self.right_successor()
+
+        return None
+
 
 
 class BinaryTree():
@@ -29,6 +84,9 @@ class BinaryTree():
 
 
     def _add(self, node, val):
+        """
+        Recursive function to add node into Binary Tree
+        """
         if val > node.data:
             if node.right == None:
                 node.right = Node(val)
@@ -53,11 +111,43 @@ class BinaryTree():
         self.count += 1
 
 
+    def _remove(self, val, node):
+        """
+        Recursive function to remove node from Binary Tree
+        """
+        if node == None:
+            return node
+
+        if val < node.data:
+            node.left = self._remove(val, node.left)
+        elif val > node.data:
+            node.right = self._remove(val, node.right)
+        else:
+            if node.left == None:
+                temp = node.right
+                node = None
+                print(val, 'LEFT is NONE')
+                return temp
+            elif node.right == None:
+                temp = node.left
+                node = None
+                print(val, 'RIGHT is NONE')
+                return temp
+
+            temp = node.right_successor()
+            print(val, temp)
+            node.data = temp.data
+            node.right = self._remove(val, node.right)
+
+        return node
+
+
+
     def remove(self, val):
         """
         Remove a Node with data `val` from the Binary Tree
         """
-        pass
+        self.root = self._remove(val, self.root)
 
 
     def _traverse(self, node, mode):
@@ -70,12 +160,12 @@ class BinaryTree():
         if mode == "inorder":
             return (
                 self._traverse(node.left, mode="inorder") +
-                [node] +
+                [node.data] +
                 self._traverse(node.right, mode="inorder")
             )
         elif mode == "preorder":
             return (
-                [node] +
+                [node.data] +
                 self._traverse(node.left, mode="preorder") +
                 self._traverse(node.right, mode="preorder")
             )
@@ -83,15 +173,40 @@ class BinaryTree():
             return (
                 self._traverse(node.left, mode="postorder") +
                 self._traverse(node.right, mode="postorder") +
-                [node]
+                [node.data]
             )
 
 
     def inorder_traversal(self):
         return self._traverse(self.root, mode="inorder")
 
+
     def preorder_traversal(self):
         return self._traverse(self.root, mode="preorder")
 
+
     def postorder_traversal(self):
         return self._traverse(self.root, mode="postorder")
+
+
+if __name__ == '__main__':
+    """
+    For Testing...
+    """
+    tree = BinaryTree()
+    tree.add(2)
+    tree.add(2)
+    tree.add(3)
+    tree.add(4)
+    tree.add(5)
+    tree.add(2.5)
+    tree.add(3.5)
+    tree.add(2.75)
+    print(tree.inorder_traversal())
+    tree.remove(4)
+    tree.remove(5)
+    tree.remove(2)
+    tree.remove(66)
+    tree.remove(2)
+    tree.remove(2.75)
+    print(tree.inorder_traversal())
